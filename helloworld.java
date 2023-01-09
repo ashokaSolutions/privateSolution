3911a7480871cf936d7117983ef6e2ecff7fa3ed
darkone03 <ahiresumedh40@gmail.com>
2023-01-09 21:02:34 +0530

    made changes in hello world

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,9 @@ class helloworld{
     public static void main(String[] args) {
         System.out.println("hello worldls");
         int a=5;
-        System.out.print(a);
+   
+	System.out.print(a);
+
+	//hello world i am here
     }
 }
