710e0f28f7e5ec9cb48ed1885e9a92d2b6ef1973
Sumedh ahire <94281812+darkone03@users.noreply.github.com>
2023-01-14 19:52:12 +0530

    a++ in the hellworld.java file

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,9 @@ class helloworld{
     public static void main(String[] args) {
         System.out.println("hello worldls");
         int a=5;
-   
+   	
+	a++;
+	    
 	System.out.print(a);
 
 	//hello world i am here
