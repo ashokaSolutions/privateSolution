e4e1dc7442054755b9b20d9d2ed0a51cced2facf
Sumedh ahire <94281812+darkone03@users.noreply.github.com>
2023-04-23 12:56:10 +0530

    Create books.php

---
+++ TybscAss/webTect/ass2/seta/c/books.php
@@ -0,0 +1,17 @@
+<?xml version='1.0' encoding='UTF-8'?>
+<BookInfo>
+  <book>
+      <bookno>1</bookno>
+      <bookname>JAVA</bookname>
+      <authorname>Balguru swami</authorname>
+      <price>250</price>
+      <year>2006</year>
+  </book>
+  <book>
+      <bookno>2</bookno>
+      <bookname>C</bookname>
+      <authorname>Denis Ritchie</authorname>
+      <price>500</price>
+      <year>1971</year>
+  </book>
+</BookInfo>
