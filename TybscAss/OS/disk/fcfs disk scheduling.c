3c8b0680d11b33461c1181f2c9c2298bc174f689
Sumedh ahire <94281812+darkone03@users.noreply.github.com>
2023-04-27 21:29:58 +0530

    Create fcfs disk scheduling.c

---
+++ TybscAss/OS/disk/fcfs disk scheduling.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+//just start subtracting on index at a time
+
+int subChecker(int x,int y){
+	if(x>y || x==y)
+		return x-y;
+	else 
+		return y-x;
+
+}
+
+void accept(int arr[],int n){
+	printf("give inputs:");
+	for(int i=0;i<n;i++){
+		printf("\ngive %d element:",i+1);
+		scanf("%d",&arr[i]);
+	}
+}
+int main(){
+	int arr[25],head,length;
+	printf("give length of the array:");
+	scanf("%d",&length);
+	accept(arr,length);
+	printf("\nGive head:");
+	scanf("%d",&head);
+	int seek=0;
+	for(int i=0;i<length;i++){
+			printf("\n%d)seek:%d\t head=%d\t arr[i]=%d\n",i,seek,head,arr[i]);
+			seek+=subChecker(head,arr[i]);
+			head=arr[i];
+	}
+	printf("Total seek:%d\n",seek);
+	
