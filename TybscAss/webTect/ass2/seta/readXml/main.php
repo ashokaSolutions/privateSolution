9a3232f8608b7ed8de59d705dd0b09604cbef486
Sumedh ahire <94281812+darkone03@users.noreply.github.com>
2023-05-09 14:48:50 +0530

    Update main.php

--- TybscAss/webTect/ass2/seta/readXml/main.php
+++ TybscAss/webTect/ass2/seta/readXml/main.php
@@ -1,28 +1,25 @@
-<html>
-    <head>
-        <style>
-            #ItemName {
-                Color : red;
-                Font-family : copperplate Gothic Light;
-                Font-size : 16pt;
-                Font :bold;
-            }
-            #ItemPrice, #Quantity{
-                color:yellow;
-                font-family:Arial;
-                font-size:12 pt;
-                font:bold;
-            }
-        </style>
-    </head>
-    <body>
-        <?php
-            $xml=simplexml_load_file("item.xml");
-             foreach($xml->Item as $item){
-                echo "<p id='ItemName'>".$item->ItemName."</p>";
-                echo "<p id='ItemPrice'>".$item->ItemPrice."</p>";
-                echo "<p id='Quantity'>".$item->Quantity."</p>";
-             }     
-        ?>   
-    </body>
-</html>
+<?php
+    //just add everything in a string
+    $items = array(
+        array("name" => "Item 1", "type" => "Electronics", "rate" => 10.99, "quantity" => 50),
+        array("name" => "Item 2", "type" => "Books", "rate" => 5.99, "quantity" => 20),
+        array("name" => "Item 3", "type" => "Clothing", "rate" => 19.99, "quantity" => 30),
+        array("name" => "Item 4", "type" => "Beauty", "rate" => 7.99, "quantity" => 40),
+        array("name" => "Item 5", "type" => "Home", "rate" => 24.99, "quantity" => 10)
+    );
+    
+    $data="<?xml version=\"1.0\" ?>\n";
+    $data.="<items>\n";
+    foreach($items as $row){
+            $data.=" <item>\n";
+            $data.="    <name>".$row["name"]."</name>\n";
+            $data.="    <type>".$row["type"]."</type>\n";
+            $data.="    <rate>".$row["rate"]."</rate>\n";
+            $data.="    <quantity>".$row["quantity"]."</quantity>\n";
+            $data.=" </item>\n";
+        
+    }
+    
+    $data.="<items>\n";
+    file_put_contents("item.xml",$data);
+?>
