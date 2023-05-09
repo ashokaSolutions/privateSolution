93dc87cf1b19014fa2a3ef1a1b4f5c207eddb82a
Sumedh ahire <94281812+darkone03@users.noreply.github.com>
2023-05-09 13:54:08 +0530

    Create display.php

---
+++ TybscAss/webtech/slip2/display.php
@@ -0,0 +1,15 @@
+<html>
+    <style>
+        body{
+            background-color: <?php echo $_COOKIE["bgcolor"] ?>;
+        }
+        p{
+            font:<?php echo $_COOKIE["font"] ?>;
+            size:<?php echo $_COOKIE["size"] ?>;
+            color: <?php echo $_COOKIE["color"] ?>;
+        }
+    </style>
+    <body>
+        <p>hello</p>
+    </body>
+</html>
